fa800936734a3637ab235ba584e96bb4ecaac410
Szabó Gergő <gergo85@gmail.com>
2017-01-26 13:07:16 +0100

    Minor improvements and bugfix

--- controllers/Statistics.php
+++ controllers/Statistics.php
@@ -5,18 +5,19 @@ use BackendMenu;
 
 class Statistics extends Controller
 {
-    public $implement = [
-        'Backend.Behaviors.FormController',
-        'Backend.Behaviors.ListController'
-    ];
-
-    public $formConfig = 'config_form.yaml';
-    public $listConfig = 'config_list.yaml';
+    public $requiredPermissions = ['indikator.blogstat.statistics'];
 
     public function __construct()
     {
         parent::__construct();
 
         BackendMenu::setContext('RainLab.Blog', 'blog', 'statistics');
+
+        $this->pageTitle = trans('indikator.blogstat::lang.menu.statistics');
+    }
+
+    public function index()
+    {
+        // ...
     }
 }
--- models/Statistics.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Indikator\BlogStat\Models;
-
-use Model;
-
-class Statistics extends Model
-{
-    protected $table = 'rainlab_blog_posts';
-}
